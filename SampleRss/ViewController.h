cb38e0420d99d9961c7c9384d6fe5a7fd75559b8
scamps <scamps1@gmail.com>
2015-02-26 11:32:11 +0100

    Initial Commit

---
+++ SampleRss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleRss
+//
+//  Created by Alberto Scampini on 26/02/15.
+//  Copyright (c) 2015 Alberto Scampini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleRss/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SampleRss
+//
+//  Created by Alberto Scampini on 26/02/15.
+//  Copyright (c) 2015 Alberto Scampini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
